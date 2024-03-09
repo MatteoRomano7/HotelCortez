40e4b3cf80c2220acfa4453139d2f1942c503d4b
Matteo <ezmatteo@aol.com>
2024-03-09 09:52:10 +0100

    Aggiungere i file di progetto.

---
+++ Models/Pensione.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Albergo.Models
+{
+    public class Pensione
+    {
+        [Key]
+        public int Pensione_ID { get; set; }
+
+
+        
+        //Tipo
+        [Required(ErrorMessage = "Campo Obbligatorio")]
+        [Display(Name = "Tipo Pensione")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "la password deve contenere meno di 50 caratteri, almeno 3")]
+        public string Tipo { get; set; }
+
+        //Supplemento
+        [Required(ErrorMessage = "Campo Obbligatorio")]
+        [Range(1, 1000000, ErrorMessage = "Max 1000000, min 10 cifre")]
+        public decimal Supplemento { get; set; }
+    }
+}
---
+++ Models/Prenotazione.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Albergo.Models
+{
+    public class Prenotazione
+    {
+        [Key]
+        public int Prenotazione_ID { get; set; }
+
+        //Data Prenotazione
+        [Required(ErrorMessage = "Campo obbligatorio")]
+        [Display(Name = "Data prenotazione")]
+
+        public DateTime Data_Pren { get; set; }
+
+
+
+        //Data Arrivo
+        [Required(ErrorMessage = "Campo obbligatorio")]
+        [Display(Name = "Data arrivo")]
+
+        public DateTime Data_Arrivo { get; set; }
+
+
+
+        //Data Partenza
+        [Required(ErrorMessage = "Campo obbligatorio")]
+        [Display(Name = "Data partenza")]
+
+        public DateTime Data_Partenza { get; set; }
+
+        //AnnoProg
+        public int AnnoProg { get; set; }
+
+        public int Pensione_ID { get; set; }
+        public int Ospite_ID { get; set; }
+        public int Camera_ID { get; set; }
+
+
+        public Pensione Pensione { get; set; }
+        public Ospite Ospite { get; set; }
+        public Camera Camera { get; set; }
+
+        public Checkout Checkout { get; set; }
+
+
+
+
+    }
+}
